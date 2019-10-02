93747550c09fce1597aa27b54c083fbb440645a7
efarhan <elias.farhan@gmail.com>
2019-10-02 15:26:20 +0200

    Add ping program to test udp

---
+++ punchthrough/ping_client.py
@@ -0,0 +1,35 @@
+import time
+from socket import *
+
+pings = 1
+
+# Send ping 10 times
+while pings < 11:
+
+    # Create a UDP socket
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    # Set a timeout value of 2 seconds
+    clientSocket.settimeout(2)
+
+    # Ping to server
+    message = 'test'
+
+    addr = ("127.0.0.1", 12000)
+
+    # Send ping
+    start = time.time()
+    clientSocket.sendto(message.encode(), addr)
+
+    # If data is received back from server, print
+    try:
+        data, server = clientSocket.recvfrom(1024)
+        end = time.time()
+        elapsed = end - start
+        print(data.decode() + " " + str(pings) + " " + str(elapsed))
+
+    # If data is not received back from server, print it has timed out
+    except timeout:
+        print('REQUEST TIMED OUT')
+
+    pings = pings + 1
